2b748d208da47d8a69a9502420fc59495fcfcf3e
wufulin <wufulinit@gmail.com>
2013-08-23 16:44:00 +0800

    init project

---
+++ ios-common-lib/ios_common_lib.h
@@ -0,0 +1,13 @@
+//
+//  ios_common_lib.h
+//  ios-common-lib
+//
+//  Created by wufulin on 13-8-23.
+//  Copyright (c) 2013年 netease. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ios_common_lib : NSObject
+
+@end
