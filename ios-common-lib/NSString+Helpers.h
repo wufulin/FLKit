4daaa391ffcffeb2e4d14a7831d4f71986b8a2aa
wufulin <wufulinit@gmail.com>
2013-08-28 15:31:11 +0800

    增加NSString(Helpers)类别

---
+++ ios-common-lib/NSString+Helpers.h
@@ -0,0 +1,57 @@
+//
+//  NSString+Helpers.h
+//  ios-common-lib
+//
+//  Created by wufulin on 13-8-28.
+//  Copyright (c) 2013年 netease. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Helpers)
+
+/// 计算字符串的MD5值.
+- (NSString *)MD5;
+
+/// 计算字符串的sha1值.
+- (NSString *)sha1;
+
+/// 反转字符串.
+- (NSString *)reverse;
+
+/// URL编码.
+- (NSString *)URLEncode;
+
+/// URL反编码.
+- (NSString *)URLDecode;
+
+/// 去掉字符串中的空格符.
+- (NSString *)stringByStrippingWhitespace;
+
+/**
+ * 从from索引到to索引处截取字符串.
+ *
+ * @param from 索引.
+ * @param to 索引.
+ * @return 返回NSString值.
+ */
+- (NSString *)substringFrom:(NSInteger)from to:(NSInteger)to;
+
+/// 大写字符串第一个字符.
+- (NSString *)CapitalizeFirst;
+
+/// 计算字符串里的单词总数.
+- (NSUInteger)countWords;
+
+/**
+ * 判断是否包指定的含字符串.
+ *
+ * @param string 指定的字符串.
+ * @return 返回BOOL值.
+ */
+- (BOOL)contains:(NSString *)string;
+
+/// 判断字符串是否为空.
+- (BOOL)isBlank;
+
+@end
---
+++ ios-common-lib/NSString+Helpers.m
@@ -0,0 +1,111 @@
+//
+//  NSString+Helpers.m
+//  ios-common-lib
+//
+//  Created by wufulin on 13-8-28.
+//  Copyright (c) 2013年 netease. All rights reserved.
+//
+
+#import "NSString+Helpers.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSString (Helpers)
+
+- (NSString *)MD5 {
+	const char *str = [self UTF8String];
+	unsigned char result[CC_MD5_DIGEST_LENGTH];
+	CC_MD5(str, strlen(str), result);
+	return [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+			result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
+			result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15]];
+}
+
+- (NSString *)sha1 {
+	const char *cstr = [self cStringUsingEncoding:NSUTF8StringEncoding];
+	NSData *data = [NSData dataWithBytes:cstr length:self.length];
+	
+	uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+	
+	CC_SHA1(data.bytes, data.length, digest);
+	
+	NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+	
+	for (int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) {
+		[output appendFormat:@"%02x",digest[i]];
+	}
+	return output;
+}
+
+- (NSString *)reverse {
+	NSInteger length = [self length];
+	unichar *buffer = calloc(length, sizeof(unichar));
+	
+	[self getCharacters:buffer range:NSMakeRange(0, length)];
+	
+	for (int i = 0, mid = ceil(length/2.0); i < mid; i++) {
+		unichar c = buffer[i];
+		buffer[i] = buffer[length-i-1];
+		buffer[length-i-1] = c;
+	}
+	
+	NSString *s = [[NSString alloc] initWithCharacters:buffer length:length];
+	buffer = nil;
+	return s;
+}
+
+- (NSUInteger)countWords {
+	__block NSUInteger wordCount = 0;
+	[self enumerateSubstringsInRange:NSMakeRange(0, self.length)
+							 options:NSStringEnumerationByWords
+						  usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+							  wordCount ++;
+						  }];
+	return wordCount;
+}
+
+- (NSString *)stringByStrippingWhitespace {
+	return [self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+
+- (NSString *)substringFrom:(NSInteger)from to:(NSInteger)to {
+	NSString * rightPart = [self substringFromIndex:from];
+	return [rightPart substringToIndex:to-from];
+}
+
+- (NSString *)URLEncode {
+	CFStringRef encoded = CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
+                                                                  (__bridge CFStringRef)self,
+                                                                  NULL,
+                                                                  CFSTR(":/?#[]@!$&'()*+,;="),
+                                                                  kCFStringEncodingUTF8);
+    return [NSString stringWithString:(__bridge_transfer NSString *)encoded];
+}
+
+- (NSString *)URLDecode {
+	CFStringRef decoded = CFURLCreateStringByReplacingPercentEscapes( kCFAllocatorDefault,
+                                                                     (__bridge CFStringRef)self,
+                                                                     CFSTR(":/?#[]@!$&'()*+,;=") );
+    return [NSString stringWithString:(__bridge_transfer NSString *)decoded];
+}
+
+- (NSString *)CapitalizeFirst {
+	if ([self length] == 0) {
+		return self;
+	}
+	return [self stringByReplacingCharactersInRange:NSMakeRange(0, 1)
+										   withString:[[self substringWithRange:NSMakeRange(0, 1)] capitalizedString]];
+}
+
+- (BOOL)isBlank {
+	if ([[self stringByStrippingWhitespace] isEqualToString:@""]) {
+		return YES;
+	}
+	return NO;
+}
+
+- (BOOL)contains:(NSString *)string {
+	NSRange range = [self rangeOfString:string];
+	return (range.location != NSNotFound);
+}
+
+@end
