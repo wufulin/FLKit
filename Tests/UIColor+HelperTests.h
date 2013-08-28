76b284f890dad8e80145b8f913420b14d857d5a7
wufulin <wufulinit@gmail.com>
2013-08-28 17:28:59 +0800

    增加UIColor(Helpers)类别的单元测试

---
+++ Tests/UIColor+HelperTests.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+HelperTests.h
+//  ios-common-lib
+//
+//  Created by wufulin on 13-8-28.
+//  Copyright (c) 2013年 netease. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UIColor_HelperTests : SenTestCase
+
+@end
---
+++ Tests/UIColor+HelperTests.m
@@ -0,0 +1,37 @@
+//
+//  UIColor+HelperTests.m
+//  ios-common-lib
+//
+//  Created by wufulin on 13-8-28.
+//  Copyright (c) 2013年 netease. All rights reserved.
+//
+
+#import "UIColor+HelperTests.h"
+#import "UIColor+Helpers.h"
+
+@interface UIColor_HelperTests () {
+	UIColor *color;
+}
+@end
+
+@implementation UIColor_HelperTests
+
+- (void)setUp
+{
+    [super setUp];
+	color = [UIColor colorWithHex:0x504333];
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    color = nil;
+    [super tearDown];
+}
+
+- (void)testColorWithHex {
+	UIColor *otherColor = RGBACOLOR(80, 67, 51, 1.0);
+	STAssertEqualObjects(color, otherColor, nil);
+}
+@end
