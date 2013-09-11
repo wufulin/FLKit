33e2b8f5867b33448de453698be40d9bf83734c2
wufulin <wufulinit@gmail.com>
2013-09-11 14:52:43 +0800

    增加Tool类

---
+++ ios-common-lib/Tool.h
@@ -0,0 +1,23 @@
+//
+//  Tool.h
+//  ios-common-lib
+//
+//  Created by wufulin on 13-9-11.
+//  Copyright (c) 2013年 netease. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// 颜色宏定义
+#define RGBACOLOR(r,g,b,a) ([UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)])
+#define RGBCOLOR(r,g,b)	([UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1.0])
+
+// 判断设备是否为iPhone5尺寸的宏定义
+#define IS_IPHONE_5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO)
+
+// 判断设备是否为iPad的宏定义
+#define IS_PAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+
+@interface Tool : NSObject
+
+@end
---
+++ ios-common-lib/Tool.m
@@ -0,0 +1,13 @@
+//
+//  Tool.m
+//  ios-common-lib
+//
+//  Created by wufulin on 13-9-11.
+//  Copyright (c) 2013年 netease. All rights reserved.
+//
+
+#import "Tool.h"
+
+@implementation Tool
+
+@end
