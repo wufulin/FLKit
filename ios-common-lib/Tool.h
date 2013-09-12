d2b444214c941a51d6e72adc2c37f666b000e0c2
wufulin <wufulinit@gmail.com>
2013-09-12 16:02:55 +0800

    Tool类添加若干宏定义

--- ios-common-lib/Tool.h
+++ ios-common-lib/Tool.h
@@ -13,6 +13,24 @@
 #define RGBACOLOR(r,g,b,a) ([UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)])
 #define RGBCOLOR(r,g,b)	([UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1.0])
 
+// 应用屏幕高度宏定义（不包含状态栏）.
+#define APP_HEIGHT (CGRectGetHeight([UIScreen mainScreen].applicationFrame))
+
+// 手机屏幕高低宏定义（包括状态栏）.
+#define SCREEN_HEIGHT (CGRectGetHeight([UIScreen mainScreen].bounds))
+
+// 状态栏高度宏定义.
+#define STATUSBAR_HEIGHT (CGRectGetHeight([[UIApplication sharedApplication] statusBarFrame]))
+
+// 导航条高度宏定义.
+#define NAVIGATIONBAR_HEIGHT 44.0
+
+// 标签栏高度宏定义.
+#define TABBAR_HEIGHT 49.0
+
+// 打印视图坐标和宽高宏定义.
+#define PRINTF(rect) NSLog(@"x = %.2f,y = %.2f,width = %.2f,height = %.2f",(rect).origin.x,(rect).origin.y,(rect).size.width,(rect).size.height)
+
 // 判断设备是否为iPhone5尺寸的宏定义·
 #define IS_IPHONE_5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO)
 
