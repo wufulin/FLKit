84260a0b919a692e4843231367725b6738b112c9
wufulin <wufulinit@gmail.com>
2013-09-25 16:35:21 +0800

    增加UIFont(Helpers)类别

---
+++ ios-common-lib/UIFont+Helpers.h
@@ -0,0 +1,18 @@
+//
+//  UIFont+Helpers.h
+//  ios-common-lib
+//
+//  Created by wufulin on 13-9-25.
+//  Copyright (c) 2013年 netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (Helpers)
+
++ (UIFont *)heiti;
++ (UIFont *)heitiWithFontSize:(CGFloat)size;
++ (UIFont *)helvetica;
++ (UIFont *)helveticaWithFontSize:(CGFloat)size;
+
+@end
